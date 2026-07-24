0f417e8cf110863ed81155b43baaf0bbe79ad79d
Akansh <palakansh439@gmail.com>
2026-07-24 12:38:55 +0530

    Implemented addmilkrecord with map of lists pattern

--- src/MilkRecordService.java
+++ src/MilkRecordService.java
@@ -7,6 +7,12 @@ public class MilkRecordService {
 
     }
     public void addmilkrecord(String customerId, milkrecord record){
-
+        if(milkrecords.containsKey(customerId)){
+             milkrecords.get(customerId).add(record);
+        }else{
+            ArrayList<milkrecord> newList= new ArrayList<>();
+            newList.add(record);
+            milkrecords.put(customerId,newList);
+        }
     }
 }
